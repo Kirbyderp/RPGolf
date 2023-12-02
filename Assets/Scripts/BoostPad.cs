8ce80cb2a130ee396e50e29fcc962ed9c55aa99a
Kirbyderp <60892148+Kirbyderp@users.noreply.github.com>
2023-12-02 15:48:30 -0500

    Boost Pad Model Prefab
    - Boost Pad Functionality has been added to the model prefab

--- Assets/Scripts/BoostPad.cs
+++ Assets/Scripts/BoostPad.cs
@@ -9,7 +9,7 @@ public class BoostPad : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        boostDirT = gameObject.GetComponentsInChildren<Transform>()[1];
+        boostDirT = gameObject.GetComponentsInChildren<BoostDir>()[0].transform;
     }
 
     // Update is called once per frame
@@ -22,12 +22,10 @@ public class BoostPad : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Golf Ball"))
         {
-            Debug.Log("Applying Force");
             other.gameObject.GetComponent<Rigidbody>().AddForce((boostDirT.position
                                                                 - transform.position).normalized
-                                                                * 10000 * Time.deltaTime,
+                                                                * 5000 * Time.deltaTime,
                                                                 ForceMode.Force);
-            Debug.Log(other.gameObject.GetComponent<Rigidbody>().velocity);
         }
     }
 }
